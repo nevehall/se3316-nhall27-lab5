d46c0af36a919f98907cadd60cf5a5bb558a3093
Neve Hall <nhall27@uwo.ca>
2018-11-29 21:57:00 +0000

    initial commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+}
